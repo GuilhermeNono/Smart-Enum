b655b0d150ca74617dfe605a30b9aba686ea0399
Guilherme Nono <guilherme.nono45789@gmail.com>
2025-05-02 16:10:14 -0300

    feat: Adding example enum

---
+++ SmartEnum/Enum/Color.cs
@@ -0,0 +1,9 @@
+﻿using SmartEnum.Common.Enum;
+
+namespace SmartEnum.Enum;
+
+public sealed class Color(string name, int value) : SmartEnum<Color>(name, value)
+{
+    public static readonly Color Red = new Color("Red", 1);
+    public static readonly Color Blue = new Color("Blue", 2);
+}
